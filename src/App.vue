a610b2870c3bd113aa1c70aadd105e66f312eb39
Talal <talalothmanse@gmail.com>
2023-03-13 03:31:03 +0800

    -Created initial functionality

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { ref, watchEffect } from 'vue'
+
+const link = ref()
+const onSubmit = () => console.log("Working...")
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <form @submit.prevent="onSubmit">
+    <input v-model="link" placeholder="Enter Link Here">
+    <button>Shorten Link</button>
+  </form>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
